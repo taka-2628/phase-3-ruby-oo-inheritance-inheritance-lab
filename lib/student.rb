bad4098f99c19f194f9311748736c85782402424
Takashi <th2826@columbia.edu>
2022-03-20 14:00:00 -0400

    completed assignment

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
-class Student 
+require_relative './user'
 
+class Student < User
+  attr_reader :knowledge
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn str
+    self.knowledge << str
+  end
 end
